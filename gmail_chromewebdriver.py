b326b5ea375a6649412e960be5abdab87f4cad92
NMarco <53913990+marco-create@users.noreply.github.com>
2020-08-05 08:58:23 +0200

    Update gmail_chromewebdriver.py

--- gmail_chromewebdriver.py
+++ gmail_chromewebdriver.py
@@ -4,7 +4,6 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import time
 
-## ASK FOR EMAIL AND PASSWORD (in this way they won't be saved in this script).
 print('Insert your email: ')
 email = input()
 print('Insert your password: ')
@@ -17,18 +16,14 @@ print('Write your text below (remember that to wrap the text, you need {} when y
 maintext = input()
 print('==='*7)
 
-## ASK FOR LOGIN VIA STACK OVERFLOW.
-## Connecting using chromewebdriver.exe causes google to block the direct login. To avoid this, login with google in Stack Overflow.
+## LOGIN VIA STACK OVERFLOW.
+## Connecting using chromewebdriver.exe causes google to block the direct login. To avoid this, login in google via Stack Overflow.
 driver = webdriver.Chrome()
-driver.get('https://stackoverflow.com/')  # get stack overflow page
-
-## Find login button and click it (located by ink text).
+driver.get('https://stackoverflow.com/')
 driver.find_element_by_link_text('Log in').click()
-time.sleep(6)   # pause to avoid CAPTCHA
-
-## Click on "login via google" in stack overflow (located by xpath).
+time.sleep(6)    # avoid reCAPCHA
 main = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="openid-buttons"]/button[1]'))).click()
-driver.implicitly_wait(5)   # driver in pause to load
+driver.implicitly_wait(5)
 
 ## LOGIN IN GOOGLE. Email box by name...
 driver.find_element_by_name('identifier').send_keys(email)
@@ -39,7 +34,6 @@ print('EMAIL: done')
 ## Depending from your settings, could be that Chrome will ask you to validate your login via mobilephone instead insert the password.
 ## If so, you need to check your phone. To avoid timing problems, let's the program wait for a while.
 driver.implicitly_wait(10)
-## now let's try to locate the password box
 try:
     driver.find_element_by_xpath('//*[@id="password"]/div[1]/div/div[1]/input').send_keys(password)
     print('PASSWORD: found box')
@@ -51,13 +45,11 @@ finally:
     pass
 time.sleep(6)
 
-## Now you should be logged in in google.
 ## Eventually, we can redirect into Gmail and locate the "Compose" button by xpath.
 driver.get('https://mail.google.com/mail/u/0/#inbox')
 WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, '//div[contains(text(),"Compose")]'))).click()
 print('NEW MAIL: pressed')
 
-## Let's create a new mail filling Recipient, Subject and mainbox. 
 try:
     recipient = WebDriverWait(driver,10).until(
                 EC.presence_of_element_located((By.XPATH, '//textarea[contains(@name,"to")]'))
@@ -77,8 +69,7 @@ try:
     driver.implicitly_wait(3)  
     main_box.send_keys(maintext.replace('\\n',' \n'))
     
-    ## Send message after waiting 20 seconds so the text will be completely written
-    time.sleep(20)
+    time.sleep(20)    # wait for the writing
     driver.find_element_by_xpath('//div[contains(text(),"Send") and @role="button"]').click()
     print("Message Sent Successfully")
 except:
