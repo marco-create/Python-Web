67224be1f5e9318b0dfa92ce2fe966ef5ce19f3d
NMarco <53913990+marco-create@users.noreply.github.com>
2020-08-11 20:33:45 +0200

    Update news_topic.py

--- API/news_topic.py
+++ API/news_topic.py
@@ -1,21 +1,34 @@
 from GoogleNews import GoogleNews
+gnews = GoogleNews(lang='en')
+run = True
 
+print('============\nGOOGLE NEWS\n============')
 main_topic = input('Choose a topic: ')
 
-gnews = GoogleNews(lang='en')
-try:
-    gnews.search(main_topic)
-    for n, result in enumerate(gnews.result()):
-        print(n, result['title'])
+while run:
+    try:
+        gnews.search(main_topic)
+        for n, result in enumerate(gnews.result()):
+            print(n, result['title'])
 
-    ## choose which article to pick
-    article = int(input('\nChoose an article by its main index: '))
-    print('Article - Title: ', gnews.gettext()[article])
-    list_artcl = gnews.gettext()
-    print('Article - Link: ', gnews.get__links()[article])
-    list_links = gnews.get__links()
-    print('==========================================================================')
-    
+        ## choose which article to pick
+        article = input('\nChoose an article by its main index or choose [all] to visualize all links: ')
+        if article == 'all':
+            [print(new) for new in gnews.gettext()]
+            [print(link) for link in gnews.get__links()]
+        else:
+            article = int(article)
+            print(f'Article - Title: {gnews.gettext()[article]}')
+            list_artcl = gnews.gettext()
+            print(f'Article - Link: {gnews.get__links()[article]}')
+            list_links = gnews.get__links()
+        print('==========================================================================\n')
+        go_further = input('DO you want to read other articles? (y/n) ').lower()
+        if go_further == 'y' or go_further == 'yes':
+            gnews.clear()   ## before going in loop, needs to clears the article list
+        else:
+            run = False ## break the loop
 
-except:
-    print('Error :S')
+    except:
+        print('Error :S')
+        run = False ## break the loop
