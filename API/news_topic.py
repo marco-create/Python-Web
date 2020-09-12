51ca4ca5a359a0deb808434eee193d94e5926578
NMarco <53913990+marco-create@users.noreply.github.com>
2020-09-12 15:32:40 +0200

    Update news_topic.py
    Messed around with the code. Working one now.

--- API/news_topic.py
+++ API/news_topic.py
@@ -1,22 +1,3 @@
-import time
-import webbrowser
-import selenium
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver import ActionChains
-
-
-def youtube_topic(yt_search):
-
-	print('============\nYOUTUBE\n============')
-	
-	PATH = "C:\Program Files (x86)\chromedriver.exe"	## chrome executable on Windows
-	options_chrome = Options()
-	options_chrome.add_argument('headless')   ## no UI  
-	#yt_search = input('What are you looking for in YouTube? ')
 from GoogleNews import GoogleNews
 gnews = GoogleNews(lang='en')
 run = True
@@ -34,6 +15,7 @@ while run:
         article = input('\nChoose an article by its main index or choose [all] to visualize all links: ')
         if article == 'all':
             [print(f'{n}: {new}') for n, new in enumerate(gnews.gettext())]
+            print('--')
             [print(f'{n}: {link}') for n, link in enumerate(gnews.get__links())]
         else:
             article = int(article)
